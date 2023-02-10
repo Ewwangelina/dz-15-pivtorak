e98741467539bba04dba2fe3c84d73665c086df9
Kseniia Pivtorak <117482774+Ewwangelina@users.noreply.github.com>
2023-02-10 22:20:32 +0200

    first commit

---
+++ dz15.js
@@ -0,0 +1,19 @@
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+//const array = [1, 2, 3, 4, 5, 6, 7]
+//removeElement(array, 5 )
+//console.log(array)
+// Результат: [1, 2, 3, 4, 6, 7]
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(array);
+
+function removeElement(array, index){
+    array.splice(index, 1);
+    return array
+}
+console.log(removeElement(array, 4));
+
+
+
